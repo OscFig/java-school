7c529f493ec4aed528cbf14b4d7ade87d5d353f8
Oscar Figueroa <Osc.Fig96@gmail.com>
2021-02-23 09:21:28 -0800

    mvp

--- schools/src/main/java/com/lambdaschool/schools/config/SwaggerWebMVC.java
+++ schools/src/main/java/com/lambdaschool/schools/config/SwaggerWebMVC.java
@@ -17,8 +17,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 
 @Configuration
-//@EnableSwagger2
-//@EnableWebMvc
 public class SwaggerWebMVC implements WebMvcConfigurer
 
 {
